2b6a43aa8b8d7a9a7e1b460e686414816eaa07ca
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-07-25 14:14:29 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes/index';
+
+const  App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
+
+export default App;
